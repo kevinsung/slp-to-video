04364fecb7ee5099d1e2532d8427e4836266b60d
Kevin J. Sung <kevinjefferysung@gmail.com>
2020-03-27 01:35:23 -0700

    process directories instead of files

---
+++ generate_set_videos.js
@@ -0,0 +1,73 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const dir = require('node-dir');
+const slp = require('slp-parser-js');
+
+const INPUT_DIRECTORY = '/home/kjs/Projects/smash/slippi/slp_files/copies/sets';
+const OUTPUT_DIRECTORY = '/home/kjs/Projects/smash/videos/sets';
+const NUM_PROCESSES = 2
+
+
+const generateReplayConfig = (file) => {
+    let game = new slp.default(file);
+    let metadata = game.getMetadata();
+    let config = {
+        mode: 'normal',
+        replay: file,
+        startFrame: -123,
+        endFrame: metadata.lastFrame,
+        isRealTimeMode: false,
+        commandId: `${crypto.randomBytes(12).toString('hex')}`
+    };
+    let configFn = file.replace(INPUT_DIRECTORY, OUTPUT_DIRECTORY);
+    let parsed = path.parse(configFn);
+    configFn = path.join(parsed.dir, `${metadata.startAt}.json`);
+    fs.mkdir(parsed.dir, { recursive: true }, (err) => {
+        fs.writeFile(configFn, JSON.stringify(config), (err) => {
+            if (err) throw err;
+        });
+    });
+}
+
+
+const generateSetReplayConfigs = (setDir) => {
+    fs.readdir(setDir, (err, files) => {
+        if (err) throw err;
+        files.forEach((file) => {
+            if (path.extname(file) == '.slp') {
+                generateReplayConfig(path.join(setDir, file));
+            }
+        });
+    });
+}
+
+
+const processSetReplayConfigs = (setDir) => {
+    fs.readdir(setDir, (err, files) => {
+        if (err) throw err;
+        files.forEach((file) => {
+            if (path.extname(file) == '.json') {
+                console.log(file);
+            }
+        });
+    });
+}
+
+
+const main = () => {
+    fs.mkdirSync(OUTPUT_DIRECTORY, { recursive: true })
+    dir.subdirs(INPUT_DIRECTORY, (err, subdirs) => {
+        if (err) throw err;
+        subdirs.forEach(generateSetReplayConfigs);
+    });
+    dir.subdirs(OUTPUT_DIRECTORY, (err, subdirs) => {
+        if (err) throw err;
+        subdirs.forEach(processSetReplayConfigs);
+    });
+}
+
+
+if (module === require.main) {
+    main();
+}
