fef4e53c3a062491c6e5447254ae45f7541151f2
Kevin J. Sung <kevinjefferysung@gmail.com>
2020-06-20 00:16:38 -0700

    update setup.sh and rename Binaries -> Overwrite

--- setup.sh
+++ setup.sh
@@ -1,24 +1,19 @@
-git clone https://github.com/kevinsung/Ishiiruka.git
-cd Ishiiruka
-git checkout 6fcbd55952f4f1d4521eddc1a9811227bbb6925c
+#!/bin/bash -e
 
+git clone https://github.com/kevinsung/Ishiiruka.git
+pushd Ishiiruka
+git checkout 6fcbd55952f4f1d4521eddc1a9811227bbb6925c
 if [ -e "build/" ]; then
 	rm -rf build/
 	mkdir build
 else
 	mkdir build
 fi
-cd build
+pushd build
 cmake -DLINUX_LOCAL_DEV=true ../
 make -j3
-touch Binaries/portable.txt
-cp -R ../Overwrite/* Binaries/
-
-curl -L -o config.tar.gz https://github.com/project-slippi/Slippi-FM-installer/raw/master/slippi-r18-playback-config.tar.gz
-tar -xzf config.tar.gz
-rm config.tar.gz
-
-cd Binaries
-curl -L -o config.tar.gz https://github.com/kevinsung/slippi-dolphin-playback-config/raw/master/r18-config.tar.gz
-tar -xzf config.tar.gz
-rm config.tar.gz
+popd
+touch build/Binaries/portable.txt
+cp -R Overwrite/* build/Binaries/
+popd
+cp -R Overwrite/* Ishiiruka/build/Binaries/
