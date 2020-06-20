b4bc1c1823bb90a83de9467305c55d80c67ede8d
Kevin J. Sung <kevinjefferysung@gmail.com>
2020-06-20 00:50:39 -0700

    remove docker stuff

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-Xvfb :1 &
-XVFB_PID=$!
-export DISPLAY=:1
-node generate_set_videos.js --dolphin_path /usr/src/slp-to-video/Ishiiruka/build/Binaries/dolphin-emu --ssbm_iso_path /usr/src/slp-to-video/SSBM.iso --input $1 --output $2 --num_cpus $3
-kill $XVFB_PID
