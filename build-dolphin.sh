61969ab16764f9bb454f5dbe47179b618936e390
Kevin J. Sung <kevjsung@umich.edu>
2021-08-16 11:28:29 -0400

    update dolphin build script

--- build-dolphin.sh
+++ build-dolphin.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -e
 
-git clone https://github.com/kevinsung/Ishiiruka.git
-pushd Ishiiruka
-git checkout 6fcbd55952f4f1d4521eddc1a9811227bbb6925c
-if [ -e "build/" ]; then
-	rm -rf build/
-	mkdir build
-else
-	mkdir build
+VERSION=2.3.2
+
+if [ -e "Ishiiruka/" ]; then
+    rm -rf Ishiiruka/
 fi
-pushd build
-cmake -DLINUX_LOCAL_DEV=true ../
-make -j$(nproc)
+
+if [ -e "Ishiiruka-$VERSION.tar.gz" ]; then
+    rm Ishiiruka-$VERSION.tar.gz
+fi
+
+curl -LJO https://github.com/project-slippi/Ishiiruka/archive/refs/tags/v$VERSION.tar.gz
+tar -xf Ishiiruka-$VERSION.tar.gz
+rm Ishiiruka-$VERSION.tar.gz
+mv Ishiiruka-$VERSION Ishiiruka
+pushd Ishiiruka
+./build-linux.sh playback
 popd
-touch build/Binaries/portable.txt
-cp -R Overwrite/* build/Binaries/
-popd
-cp -R Overwrite/* Ishiiruka/build/Binaries/
