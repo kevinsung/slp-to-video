9b1529f15de8ad0c5b83a311ca2b7770d26497a8
Kevin J. Sung <kevjsung@umich.edu>
2021-08-16 21:38:32 -0400

    add gecko codes and tidy build script

--- build-dolphin.sh
+++ build-dolphin.sh
@@ -2,19 +2,23 @@
 
 VERSION=2.3.2
 
+SOURCE=https://github.com/project-slippi/Ishiiruka/archive/refs/tags/v$VERSION.tar.gz
+BASENAME=Ishiiruka-$VERSION
+
 if [ -e "Ishiiruka/" ]; then
     rm -rf Ishiiruka/
 fi
 
-if [ -e "Ishiiruka-$VERSION.tar.gz" ]; then
-    rm Ishiiruka-$VERSION.tar.gz
+if [ -e "$BASENAME.tar.gz" ]; then
+    rm $BASENAME.tar.gz
 fi
 
-curl -LJO https://github.com/project-slippi/Ishiiruka/archive/refs/tags/v$VERSION.tar.gz
-tar -xf Ishiiruka-$VERSION.tar.gz
-rm Ishiiruka-$VERSION.tar.gz
-mv Ishiiruka-$VERSION Ishiiruka
+curl -LJO $SOURCE
+tar -xf $BASENAME.tar.gz
+rm $BASENAME.tar.gz
+mv $BASENAME Ishiiruka
 pushd Ishiiruka
 ./build-linux.sh playback
 popd
 cp -r config/* Ishiiruka/build/Binaries/
+cat gecko_codes.txt | tee -a Ishiiruka/build/Binaries/Sys/GameSettings/GALE01r2.ini
